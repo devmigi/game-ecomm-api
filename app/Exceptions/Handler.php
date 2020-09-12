51cc972f6a6ee19dbf396e714c86e44536bbff7c
Mrigendra <mrgndrakr@live.com>
2020-09-12 21:54:36 +0530

    custom exception handler for api

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,6 +3,10 @@
 namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Throwable;
+use Illuminate\Auth\AuthenticationException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
 
 class Handler extends ExceptionHandler
 {
@@ -32,6 +36,32 @@ class Handler extends ExceptionHandler
      */
     public function register()
     {
-        //
+        $this->renderable(function (Throwable $exception, $request) {
+
+            // Api error response
+            $apiErrorResponse = [
+                'message' => 'Server Error!',
+                'code' => 500
+            ];
+
+            if ($exception instanceof NotFoundHttpException) {
+                $apiErrorResponse['message'] = 'Invalid Url!';
+                $apiErrorResponse['code'] = 404;
+            }
+            else if ($exception instanceof AuthenticationException) {
+                $apiErrorResponse['message'] = 'Unauthenticated';
+                $apiErrorResponse['code'] = 403;
+            }
+
+            // If debug mode is enabled
+            if (config('app.debug')) {
+                // Add the exception class name, message and stack trace to response
+                $apiErrorResponse['exception'] = get_class($exception); // Reflection might be better here
+                $apiErrorResponse['message'] = $exception->getMessage();
+            }
+
+            return response()->json($apiErrorResponse, $apiErrorResponse['code']);
+
+        });
     }
 }
